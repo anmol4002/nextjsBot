c2f0013f96c2bdd77fce617931b628b3cd3dae52
anmol4002 <anmolkaurbenipal4002@gmail.com>
2025-04-02 10:02:14 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- 
+ runtime: 'edge',
   experimental: {
-    runtime: 'edge', 
+     
     serverComponentsExternalPackages: ['jsonwebtoken'],
   },
   images: {
