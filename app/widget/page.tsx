7fa137f2157bedf3337c9f282710ac43cbd99676
anmol4002 <anmolkaurbenipal4002@gmail.com>
2025-04-21 11:31:45 +0530

    Updating Widget page.tsx

--- app/widget/page.tsx
+++ app/widget/page.tsx
@@ -1,42 +1,24 @@
 
-// "use client";
-
-// import dynamic from "next/dynamic";
-// import { Suspense } from "react";
-
-
-// const ChatWidget = dynamic(() => import("@/app/page"), {
-//   ssr: false,
-//   loading: () => <div className="flex items-center justify-center h-16 w-16 rounded-full bg-blue-100 animate-pulse"></div>
-// });
-
-// export default function WidgetPage() {
-//   return (
-//     <div className="w-full h-full bg-transparent">
-//       <Suspense fallback={<div className="flex items-center justify-center h-16 w-16 rounded-full bg-blue-100 animate-pulse"></div>}>
-//         <ChatWidget />
-//       </Suspense>
-//     </div>
-//   );
-// }
-
-
-
 "use client";
+
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
+
 const ChatWidget = dynamic(() => import("@/app/page"), {
   ssr: false,
-  loading: () => <div ></div>
+  loading: () => <div className="flex items-center justify-center h-16 w-16 rounded-full bg-blue-100 animate-pulse"></div>
 });
 
 export default function WidgetPage() {
   return (
-    <div className="w-full h-full bg-transparent relative">
-      <Suspense fallback={<div></div>}>
+    <div className="w-full h-full bg-transparent">
+      <Suspense fallback={<div className="flex items-center justify-center h-16 w-16 rounded-full bg-blue-100 animate-pulse"></div>}>
         <ChatWidget />
       </Suspense>
     </div>
   );
 }
+
+
+
