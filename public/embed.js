acf0ff82cbb21a5e7b78d5d49c1997e873794760
anmol4002 <anmolkaurbenipal4002@gmail.com>
2025-04-22 16:44:32 +0530

    Update embed.js

--- public/embed.js
+++ public/embed.js
@@ -1,3 +1,72 @@
+// (function() {
+  
+//   var container = document.createElement('div');
+//   container.id = 'punjab-bot-container';
+//   container.style.position = 'fixed';
+//   container.style.bottom = '0';
+//   container.style.right = '0';
+//   container.style.width = '80px'; 
+//   container.style.height = '80px';
+//   container.style.pointerEvents = 'none'; 
+//   container.style.zIndex = '9999';
+//   container.style.transition = 'all 0.4s ease-in-out';
+
+//   var iframe = document.createElement('iframe');
+//   iframe.id = 'punjab-bot-iframe';
+//   iframe.src = 'https://nextjs-bot-ten.vercel.app/widget';
+//   iframe.style.width = '100%';
+//   iframe.style.height = '100%';
+//   iframe.style.border = 'none';
+//   iframe.style.background = 'transparent';
+//   iframe.style.pointerEvents = 'auto'; 
+//   iframe.title = 'Punjab Government Chatbot';
+  
+  
+//   container.appendChild(iframe);
+//   document.body.appendChild(container);
+  
+  
+//   window.addEventListener('message', function(event) {
+  
+//     if (event.origin !== 'https://nextjs-bot-ten.vercel.app' && event.origin !== window.location.origin) return;
+    
+   
+//     if (event.data && event.data.type === 'widgetState') {
+//       switch(event.data.state) {
+//         case 'icon':
+//           container.style.width = '80px';
+//           container.style.height = '80px';
+//           break;
+//         case 'icons':
+//           container.style.width = '600px';
+//           container.style.maxWidth = '95%';
+//           container.style.height = '80px';
+//           break;
+//         case 'chat': 
+//           container.style.width = '600px';
+//           container.style.maxWidth = '95%';
+//           container.style.height = '800px';
+//           container.style.maxHeight = '95%';
+//           break;
+//         case 'maximized':
+//           container.style.width = '100%';
+//           container.style.height = '100%';
+//           container.style.right = '0';
+//           container.style.bottom = '0';
+//           container.style.maxWidth = '100%';
+//           container.style.maxHeight = '100%';
+//           break;
+//         case 'qr':
+          
+//           container.style.width = '600px';
+//           container.style.maxWidth = '95%';
+//           container.style.height = '700px';
+//           break;
+//       }
+//     }
+//   });
+// })(); 
+
 (function() {
   
   var container = document.createElement('div');
@@ -38,12 +107,12 @@
           container.style.height = '80px';
           break;
         case 'icons':
-          container.style.width = '600px';
+          container.style.width = '700px';
           container.style.maxWidth = '95%';
           container.style.height = '80px';
           break;
         case 'chat': 
-          container.style.width = '600px';
+          container.style.width = '700px';
           container.style.maxWidth = '95%';
           container.style.height = '800px';
           container.style.maxHeight = '95%';
@@ -58,12 +127,11 @@
           break;
         case 'qr':
           
-          container.style.width = '600px';
+          container.style.width = '700px';
           container.style.maxWidth = '95%';
-          container.style.height = '700px';
+          container.style.height = '800px';
           break;
       }
     }
   });
 })(); 
-
