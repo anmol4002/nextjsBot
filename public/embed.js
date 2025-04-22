b553235d101e50f5fb4c348b56b387899b442eb3
anmol4002 <anmolkaurbenipal4002@gmail.com>
2025-04-22 10:05:59 +0530

    Update embed.js

--- public/embed.js
+++ public/embed.js
@@ -4,14 +4,11 @@
   var iframe = document.createElement('iframe');
   iframe.src = 'https://nextjs-bot-ten.vercel.app/widget';
   iframe.style.position = 'fixed';
-  // iframe.style.bottom = '0';
-  // iframe.style.right = '2px';
-  // iframe.style.width = '100vh'; 
-  // iframe.style.height = '100vh'; 
-  iframe.style.bottom = '0';  
-  iframe.style.right = '2px';   
-  iframe.style.width = '650px';  
-  iframe.style.height = '500px';
+  iframe.style.bottom = '0';
+  iframe.style.right = '2px';
+  iframe.style.width = '100%'; 
+  iframe.style.height = '100%'; 
+  
   iframe.style.border = 'none';
   iframe.style.background = 'transparent';
   iframe.style.zIndex = '9999';
