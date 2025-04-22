2a9342758436f5ec76727b9029384143a1f0ff5c
anmol4002 <anmolkaurbenipal4002@gmail.com>
2025-04-22 11:02:34 +0530

    Update embed.js

--- public/embed.js
+++ public/embed.js
@@ -1,3 +1,28 @@
+// (function() {
+ 
+//   var iframe = document.createElement('iframe');
+//   iframe.src = 'https://nextjs-bot-ten.vercel.app/widget';
+//   iframe.style.position = 'fixed';
+//   iframe.style.bottom = '0';
+//   iframe.style.right = '2px';
+//   iframe.style.width = '100%'; 
+//   iframe.style.height = '100%'; 
+  
+//   iframe.style.border = 'none';
+//   iframe.style.background = 'transparent';
+//   iframe.style.zIndex = '9999';
+//   iframe.title = 'Punjab Government Chatbot';
+  
+//   // Append the iframe to the body
+//   document.body.appendChild(iframe);
+// })();
+
+
+
+
+
+
+
 (function() {
  
   var iframe = document.createElement('iframe');
@@ -5,8 +30,8 @@
   iframe.style.position = 'fixed';
   iframe.style.bottom = '0';
   iframe.style.right = '2px';
-  iframe.style.width = '100%'; 
-  iframe.style.height = '100%'; 
+  iframe.style.width = '100vh'; 
+  iframe.style.height = '100vh'; 
   
   iframe.style.border = 'none';
   iframe.style.background = 'transparent';
@@ -16,14 +41,6 @@
   // Append the iframe to the body
   document.body.appendChild(iframe);
 })();
-
-
-
-
-
-
-
-
 
 
 
