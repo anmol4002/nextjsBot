01fb0b2454c0ffaf2e9dcd23b5674832c8d0ec1a
anmol4002 <anmolkaurbenipal4002@gmail.com>
2025-04-24 10:25:57 +0530

    Update CardContent.tsx

--- components/Card/CardContent.tsx
+++ components/Card/CardContent.tsx
@@ -58,7 +58,7 @@ export default function CardContent({
     <ScrollArea
       ref={chatContainerRef}
       className={`${
-        isMaximized ? "h-[calc(100vh-150px)]" : "h-[300px]"
+        isMaximized ? "h-[calc(100vh-180px)]" : "h-[340px]"
       } pl-4 overflow-y-auto w-full chat-container`}
       style={{
         scrollbarGutter: "stable",
