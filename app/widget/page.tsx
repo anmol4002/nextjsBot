fc8fcc31a7b249c91eb9924a9d875420c9b28cf3
anmol4002 <anmolkaurbenipal4002@gmail.com>
2025-04-24 16:16:00 +0530

    Update page.tsx

--- app/widget/page.tsx
+++ app/widget/page.tsx
@@ -1,24 +1,3 @@
-// "use client";
-// import dynamic from "next/dynamic";
-// import { Suspense } from "react";
-
-// const ChatWidget = dynamic(() => import("@/app/page"), {
-//   ssr: false,
-//   loading: () => <div className="text-blue-500 font-normal">Loading...</div>
-// });
-
-// export default function WidgetPage() {
-//   return (
-//     <div className="w-full h-full bg-transparent">
-//       <Suspense fallback={<div className="text-blue-500 font-normal">Loading...</div>}>
-//         <ChatWidget />
-//       </Suspense>
-//     </div>
-//   );
-// }
-
-
-
 "use client";
 import Chat from "@/app/page";
 
