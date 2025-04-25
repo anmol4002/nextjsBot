7fd17ce4c04d631c45d179e206709d3fb35fbc09
anmol4002 <anmolkaurbenipal4002@gmail.com>
2025-04-25 15:46:18 +0530

    Update page.tsx

--- app/widget/page.tsx
+++ app/widget/page.tsx
@@ -1,6 +1,6 @@
-"use client";
-import Chat from "@/app/page";
+// "use client";
+// import Chat from "@/app/page";
 
-export default function Widget() {
-  return <Chat />;
-}
+// export default function Widget() {
+//   return <Chat />;
+// }
