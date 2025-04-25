c03bb06f0ca994e81dc4a68b10b1171dc0980cbe
anmol4002 <anmolkaurbenipal4002@gmail.com>
2025-04-25 15:26:26 +0530

    Update embed.js

--- public/embed.js
+++ public/embed.js
@@ -1,6 +1,59 @@
+// (function () {
+//   var iframe = document.createElement('iframe');
+//   iframe.src = 'https://nextjs-bot-ten.vercel.app/widget';
+//   iframe.style.position = 'fixed';
+//   iframe.style.bottom = '0';
+//   iframe.style.right = '0';
+//   iframe.style.width = '150px';
+//   iframe.style.height = '150px';
+//   iframe.style.border = 'none';
+//   iframe.style.background = 'transparent';
+//   iframe.style.zIndex = '99999';
+//   iframe.style.transition = 'all 0.3s ease';
+//   iframe.title = 'Punjab Government Chatbot';
+//   iframe.allowTransparency = 'true';
+//   iframe.className = 'chatbotFrame';
+//   iframe.setAttribute('frameborder', '0');
+//   iframe.setAttribute('scrolling', 'yes');
+
+//   document.body.appendChild(iframe);
+
+//   function handleMessage(event) {
+//     if (event.data.type === 'widgetState') {
+//       switch (event.data.state) {
+//         case 'chatOpen':
+//           iframe.style.width = '100%';
+//           iframe.style.height = '100%';
+//           break;
+//         case 'showIcons':
+//           iframe.style.width = '600px';
+//           iframe.style.height = '150px';
+//           break;
+//         default:
+//           iframe.style.width = '150px';
+//           iframe.style.height = '150px';
+//       }
+//     }
+//   }
+
+//   function handleClickOutside(event) {
+//     if (!iframe.contains(event.target)) {
+//       iframe.contentWindow.postMessage({
+//         type: 'closeWidget'
+//       }, '*');
+//     }
+//   }
+
+//   window.addEventListener('message', handleMessage);
+//   document.addEventListener('click', handleClickOutside);
+// })();
+
+
+
+
 (function () {
   var iframe = document.createElement('iframe');
-  iframe.src = 'https://nextjs-bot-ten.vercel.app/widget';
+  iframe.src = 'https://nextjs-bot-ten.vercel.app/'; 
   iframe.style.position = 'fixed';
   iframe.style.bottom = '0';
   iframe.style.right = '0';
